d94ada84a9805fd5bd7362d3c4a231f6b81022cb
Alex Marquez <70955507+AlexMarquez-coder@users.noreply.github.com>
2020-10-30 12:15:28 +0100

    Add files via upload

--- Tekstbased applicatie in Python.py
+++ Tekstbased applicatie in Python.py
@@ -18,6 +18,7 @@ def verhaalstukje1():
 
 
 def verhaalstukje2():
+    print("")
     print("Naar buiten gaan")
     print ("A. Kijken waar de schoten van daan komen")
     print ("B. Vluchten")
@@ -30,6 +31,7 @@ def verhaalstukje2():
         print("Je kunt alleen antwoorden met A of B.")
 
 def verhaalstukje4():
+    print("")
     print("Je bent gespot")
     print("")
     time.sleep(1)
@@ -38,8 +40,9 @@ def verhaalstukje4():
 
 
 def verhaalstukje5():
-    print ("vluchten")
-    print ("A. middag")
+    print("")
+    print ("Vluchten")
+    print ("A. Middag")
     print ("B. Avond")
     antwoord= input("Maak een keuze, A of B? ")
     if antwoord=="A":
@@ -51,14 +54,23 @@ def verhaalstukje5():
         print("Je kunt alleen antwoorden met A of B.")
         
 def verhaalstukje8():
-    print(" je bent gespot ")
+    print("")
+    print("Je bent gespot")
     print("")
     time.sleep(1)
     Verhaal8 = input("Game over")
-        
+
+def verhaalstukje3():
+    print("")
+    print("Binnenen blijeven")
+    print("")
+    print("Bellen voor hulp")
+    time.sleep(1)
+    verhaalstukje16()
 
 
 def verhaalstukje11():
+    print("")
     print ("Avond, hoe ga je?")
     print ("A. Auto")
     print ("B. Lopen")
@@ -73,24 +85,38 @@ def verhaalstukje11():
         
         
 def verhaalstukje13():
-    print("autorujdeN")
-    print(" je bent gespot ")
+    print("")
+    print("Je gaat autorijden")
+    print("Je bent gespot ")
     print("")
     time.sleep(1)
     Verhaal8 = input("Game over")
     
 def verhaalstukje15():
+    print("")
     print("Lopen ")
     print("")
-    print("Je bent op de grens van de Turkaje")
+    print("Je bent op de grens van de Turkije")
     time.sleep(1)
-    print("Je zit in het vleigtuig naar Amsterdam")
-    Verhaal4 = input("Je bent in veilig NedrelandS")
-    
-def verhaalstukje20(): # geen elektriciteit
-    print("geeen elektriciteti ")
     print("")
-    verhaalstukje5()
+    print("Je zit in het vleigtuig naar Amsterdam")
+    time.sleep(1)
+    print("")
+    Verhaal4 = input("Je bent in veilig Nederland")
+    time.sleep(1)
+    
+
+def verhaalstukje16():
+    print("")
+    print("Je hebt geen electriciteit wil je poberen te vluchten of het optegeven")
+    print ("A. vluchten")
+    print ("B. opgeven")
+    verhaal10 = input("Maak een keuze, A of B? ")
+    if verhaal10 == "A":
+        print("")
+        verhaalstukje5()
+    elif verhaal10 == "B":
+        print("Opgeven en niks doen")
     
 
 # hier start de  main
